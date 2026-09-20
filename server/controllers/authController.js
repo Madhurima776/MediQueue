802c3c8e433c0c16f00db19f7a3f40a9a5ecaeb8
Madhurima776 <madhu776nissy@gmail.com>
2026-09-20 18:51:16 +0530

    Change error status from 400 to 401 for auth failures

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -44,7 +44,7 @@ const login = async (req, res) => {
     const user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(400).json({
+      return res.status(401).json({
         message: "Invalid email or password"
       });
     }
@@ -52,7 +52,7 @@ const login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return res.status(400).json({
+      return res.status(401).json({
         message: "Invalid email or password"
       });
     }
