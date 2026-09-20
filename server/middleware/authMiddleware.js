91e2ca7bef3ae5312b9e2688bb1a388481bbefe2
Madhurima776 <madhu776nissy@gmail.com>
2026-09-20 19:03:20 +0530

    Refactor authMiddleware to use authHeader

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,17 +1,19 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-  if (!token) {
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({
       message: "No token provided"
     });
   }
 
+  const token = authHeader.split(" ")[1];
+
   try {
     const decoded = jwt.verify(
-      token.split(" ")[1],
+      token,
       process.env.JWT_SECRET
     );
 
